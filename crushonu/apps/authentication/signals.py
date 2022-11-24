80cc15c53b13e0165e49a3dd169e07b4ca014424
Fernando Leite <fernandogleite@gmail.com>
2022-11-24 00:35:26 -0300

    Criação de envio de email para confirmação de conta

--- crushonu/apps/authentication/signals.py
+++ crushonu/apps/authentication/signals.py
@@ -1,9 +1,29 @@
-from crushonu.apps.authentication.models import UserPhoto
+from crushonu.apps.authentication.models import (
+    UserPhoto,
+    UserConfirm
+)
+from crushonu.apps.authentication.tasks import task_send_email
 
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, post_save
 from django.dispatch import receiver
 
 
 @receiver(pre_delete, sender=UserPhoto)
 def delete_user_photo_file(sender, instance, **kwargs):
     instance.photo.delete(False)
+
+
+@receiver(post_save, sender=UserConfirm)
+def send_email_confirmation(sender, instance, created, **kwargs):
+    if created:
+        data = {
+            "name": instance.user.first_name,
+            "link": f"http://localhost:8000/authentication/user-confirm/{instance.identification_code}/"
+        }
+
+        task_send_email.delay(
+            subject="Confirmação de email",
+            to=instance.user.email,
+            template="authentication/email_confirmation.html",
+            data=data
+        )
