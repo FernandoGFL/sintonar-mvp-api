28a9865347a05957ebe706e88daa50e25a2de4d5
Fernando Leite <fernandogleite@gmail.com>
2022-11-24 00:34:47 -0300

    Adição do celery no projeto

--- crushonu/settings.py
+++ crushonu/settings.py
@@ -181,3 +181,11 @@ SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True
 }
+
+
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'America/Fortaleza'
