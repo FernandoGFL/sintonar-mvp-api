459d8fb87e7e6f9ff1042d5c5eff2d38072d1263
Fernando Leite <fernandogleite@gmail.com>
2022-11-24 00:53:34 -0300

    Mudança em como é passado o link para confirmação de email

--- crushonu/apps/authentication/signals.py
+++ crushonu/apps/authentication/signals.py
@@ -4,6 +4,8 @@ from crushonu.apps.authentication.models import (
 )
 from crushonu.apps.authentication.tasks import task_send_email
 
+from rest_framework.reverse import reverse
+
 from django.db.models.signals import pre_delete, post_save
 from django.dispatch import receiver
 
@@ -16,9 +18,16 @@ def delete_user_photo_file(sender, instance, **kwargs):
 @receiver(post_save, sender=UserConfirm)
 def send_email_confirmation(sender, instance, created, **kwargs):
     if created:
+        url = reverse(
+            'user_confirm',
+            kwargs={
+                'uuid': instance.identification_code
+            }
+        )
+
         data = {
             "name": instance.user.first_name,
-            "link": f"http://localhost:8000/authentication/user-confirm/{instance.identification_code}/"
+            "link": "http://localhost:8000" + url
         }
 
         task_send_email.delay(
