8604b47ccd65967b99acc304857e836c5a037a86
Fernando Leite <fernandogfleite@gmail.com>
2023-03-04 12:01:22 -0300

    Ajuste na deleção de foto e ajuste no envio de email de recuperação de senha

--- crushonu/apps/authentication/signals.py
+++ crushonu/apps/authentication/signals.py
@@ -14,20 +14,20 @@ from decouple import config
 
 @receiver(pre_delete, sender=UserPhoto)
 def delete_user_photo_file(sender, instance, **kwargs):
-    instance.photo.delete(False)
+    instance.photos.delete(False)
 
 
 @receiver(post_save, sender=UserConfirm)
 def send_email_confirmation(sender, instance, created, **kwargs):
     if created:
         data = {
-            "name": instance.user.first_name,
+            "name": instance.first_name,
             "link": config('CONFIRM_EMAIL_URL') + f"?identification_code={instance.identification_code}",
         }
 
         task_send_email.delay(
             subject="Confirmação de email",
-            to=instance.user.email,
+            to=instance.email,
             template="authentication/email_confirmation.html",
             data=data
         )
@@ -35,15 +35,14 @@ def send_email_confirmation(sender, instance, created, **kwargs):
 
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
-
     data = {
-        "name": instance.user.first_name,
+        "name": reset_password_token.user.first_name,
         "link": config('RESET_PASSWORD_URL') + f"?token={reset_password_token.key}",
     }
 
     task_send_email.delay(
         subject="Recuperação de senha",
-        to=instance.user.email,
+        to=reset_password_token.user.email,
         template="authentication/email_confirmation.html",
         data=data
     )
