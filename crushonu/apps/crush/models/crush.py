c2b12393eb5c86079cf69738457de1431741ce3d
Fernando Leite <fernandogfleite@gmail.com>
2023-03-09 12:03:11 -0300

    Mudança do id sequencial para uuid

--- crushonu/apps/crush/models/crush.py
+++ crushonu/apps/crush/models/crush.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+from uuid import uuid4
+
 
 User = get_user_model()
 
 
 class Crush(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     user_from = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='user_from')
     user_to = models.ForeignKey(
