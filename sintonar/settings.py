bd8169a726253323dcc36b749a7118c7509da760
Fernando Leite <fernandogfleite@gmail.com>
2024-08-06 00:23:57 -0300

    feat: Add django.contrib.postgres to INSTALLED_APPS

--- sintonar/settings.py
+++ sintonar/settings.py
@@ -51,6 +51,7 @@ X_FRAME_OPTIONS = "SAMEORIGIN"
 # Application definition
 
 INSTALLED_APPS = [
+    "django.contrib.postgres",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
